362f30c5238905dd889397688b1f7bf1e23d9a14
Jesus Agustin Peña Meza <jesus@100ladrillos.com>
2022-07-27 22:52:18 -0500

    [LWW-03] Crreate Wallet

---
+++ src/pages/Wallets/Wallets.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import Button from 'react-bootstrap/Button';
+
+import toastParams from '../../toastParams';
+import WalletForm from '../../components/WalletForm';
+import { Modal } from 'react-bootstrap';
+import * as api from './api';
+import '../../App.css';
+
+function Wallets() {
+  const [showModal, setShowModal] = useState(false);
+  const [name, setName] = useState('');
+
+  const onCreateWalletClick = () => setShowModal(true);
+
+  const createWallet = async () => {
+    let data;
+
+    try {
+      data = await api.createWallet({ name });
+
+      setShowModal(false);
+
+      toast.success(`Wallet ${data.name} created successfully`, toastParams);
+    } catch (error) {
+      const { code } = error.response.data;
+
+      if (code === 'NAME_MISSING') {
+        toast.error('Name is required', toastParams);
+      } else {
+        toast.error('There\'s a problem wi¡th the server, please contact support', toastParams);
+      }
+    }
+
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div className='wallets'>
+      <Button
+        variant="primary"
+        size="lg"
+        onClick={onCreateWalletClick}
+      >
+        Create Wallet
+      </Button>
+
+      <Modal show={showModal} onHide={handleCloseModal} >
+        <Modal.Header closeButton>
+          <Modal.Title>Create Wallet</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <WalletForm onNameChange={(value) => setName(value)} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={createWallet}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  )
+}
+
+export default Wallets
